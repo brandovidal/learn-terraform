40f6b2e4833ec4d57d01af6ca9ce52e189f7aa79
brando vidal <yisusdracon@gmail.com>
2024-01-10 17:03:55 -0500

    🔨  implement bucket

---
+++ backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "s3" {
+    bucket         = "terraform-codely-tf-states"
+    key            = "apps/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+  }
+}

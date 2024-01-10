a102304dcc83da7051e9c9ab7af2debe10c8fdb3
brando vidal <yisusdracon@gmail.com>
2024-01-10 18:55:12 -0500

    🔨  move infra to folders

--- terraform/dev/services/catalog/storage/dynamodb/backend.tf
+++ terraform/dev/services/catalog/storage/dynamodb/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket  = "terraform-codely-tf-states"
-    key     = "apps/terraform.tfstate"
+    key     = "dev/services/catalog/storage/dynamodb/terraform.tfstate"
     region  = "us-east-1"
     encrypt = true
   }

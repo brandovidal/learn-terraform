cf2bfc9aa0f011cd018f45c4757554e04729af87
Brando Vidal <yisusdracon@gmail.com>
2024-01-14 17:58:09 -0500

    🔨  export infra in files

--- projects/services/backoffice/src/api-gateway.tf
+++ projects/services/backoffice/src/api-gateway.tf
@@ -1,78 +1,8 @@
-resource "aws_s3_bucket" "lambda_bucket" {
-  bucket        = var.bucket_name
-  force_destroy = true
-}
 
-resource "aws_s3_bucket_public_access_block" "lambda_bucket" {
-  bucket = aws_s3_bucket.lambda_bucket.id
-
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-}
-
-data "archive_file" "handler" {
-  type        = "zip"
-  source_dir  = var.handler_dir
-  output_path = "handler.zip"
-}
-
-resource "aws_s3_object" "handler" {
-  bucket = aws_s3_bucket.lambda_bucket.id
-  key    = "${var.env}/${var.bucket_name}/handler.zip"
-  source = data.archive_file.handler.output_path
-  etag   = filemd5(data.archive_file.handler.output_path)
-}
-
-#  AWS Lambda
-resource "aws_iam_role" "handler_lambda_role" {
-  name = var.lambda_role
-  # name = "handler-lambda"
-
-  assume_role_policy = <<POLICY
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "lambda.amazonaws.com"
-      },
-      "Action": "sts:AssumeRole"
-    }
-  ]
-}
-POLICY
-}
-resource "aws_iam_role_policy_attachment" "handler_lambda_policy" {
-  role       = aws_iam_role.handler_lambda_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
-}
-
-resource "aws_lambda_function" "handler_api" {
-  function_name = "${var.function_name}-${var.env}"
-  # function_name = "handler"
-
-  s3_bucket = aws_s3_bucket.lambda_bucket.id
-  s3_key    = aws_s3_object.handler.key
-
-  runtime = "nodejs18.x"
-  handler = "lambda.handler"
-
-  source_code_hash = data.archive_file.handler.output_base64sha256
-
-  role = aws_iam_role.handler_lambda_role.arn
-}
-
-
-resource "aws_cloudwatch_log_group" "handler_lambda" {
-  name = "/aws/lambda/${aws_lambda_function.handler_api.function_name}-${var.env}"
-}
 
 # AWS API Gateway
 resource "aws_apigatewayv2_api" "serverless_api" {
-  name          = var.apigateway_name
+  name = var.apigateway_name
   # name          = "main"
   protocol_type = "HTTP"
 
@@ -118,7 +48,7 @@ resource "aws_apigatewayv2_integration" "lambda_handler" {
 
   integration_type = "AWS_PROXY"
   # integration_method = "ANY"
-  integration_uri  = aws_lambda_function.handler_api.invoke_arn
+  integration_uri        = aws_lambda_function.handler_api.invoke_arn
   payload_format_version = "2.0"
 }
 
@@ -129,7 +59,13 @@ resource "aws_apigatewayv2_route" "post_handler" {
   target = "integrations/${aws_apigatewayv2_integration.lambda_handler.id}"
 }
 
-resource "aws_lambda_permission" "api_gw" {
+output "base_url" {
+  description = "Base URL for API Gateway stage."
+
+  value = aws_apigatewayv2_stage.serverless_stage.invoke_url
+}
+
+resource "aws_lambda_permission" "handler_lambda_permission" {
   statement_id  = "AllowExecutionFromAPIGateway"
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.handler_api.function_name
--- projects/services/backoffice/src/lambda.tf
+++ projects/services/backoffice/src/lambda.tf
@@ -1,78 +1,49 @@
-# resource "aws_lambda_function" "catalog-writer" {
-#   # If the file is not in the current working directory you will need to include a
-#   # path.module in the filename.
-#   filename      = "catalog-writer.zip"
-#   function_name = "catalog-writer"
-#   role          = aws_iam_role.iam_for_lambda.arn
-#   handler       = "code.lambda_handler"
+#  AWS Lambda
+resource "aws_iam_role" "handler_lambda_role" {
+  name = var.lambda_role
+  # name = "handler-lambda"
 
-#   source_code_hash = filebase64sha256("catalog-writer.zip")
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
+}
+resource "aws_iam_role_policy_attachment" "handler_lambda_policy" {
+  role       = aws_iam_role.handler_lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
 
-#   runtime = "python3.9"
-# }
+resource "aws_lambda_function" "handler_api" {
+  function_name = "${var.function_name}-${var.env}"
+  # function_name = "handler"
 
-# // get dynamodb ARN from name
-# data "aws_dynamodb_table" "tableName" {
-#   name = var.dynamodb_name
-# }
+  s3_bucket = aws_s3_bucket.lambda_bucket.id
+  s3_key    = aws_s3_object.handler.key
 
-# // dynamodb table Write Policy
-# data "aws_iam_policy_document" "inline_policy" {
-#   statement {
-#     actions = [
-#       "dynamodb:DeleteItem",
-#       "dynamodb:DescribeTable",
-#       "dynamodb:GetItem",
-#       "dynamodb:GetRecords",
-#       "dynamodb:ListTables",
-#       "dynamodb:PutItem",
-#       "dynamodb:Query",
-#       "dynamodb:Scan",
-#       "dynamodb:UpdateItem",
-#       "dynamodb:UpdateTable",
-#     ]
+  runtime = "nodejs18.x"
+  handler = "lambda.handler"
 
-#     resources = [data.aws_dynamodb_table.tableName.arn]
+  source_code_hash = data.archive_file.handler.output_base64sha256
 
-#     effect = "Allow"
-#   }
-# }
+  role = aws_iam_role.handler_lambda_role.arn
+}
 
-# // IAM role used by lambda function
-# resource "aws_iam_role" "iam_for_lambda" {
-#   name = "lambda-role"
 
-#   inline_policy {
-#     name   = "policy-dynamodb-writer"
-#     policy = data.aws_iam_policy_document.inline_policy.json
-#   }
-
-#   assume_role_policy = <<EOF
-# {
-#   "Version": "2012-10-17",
-#   "Statement": [
-#     {
-#       "Action": "sts:AssumeRole",
-#       "Principal": {
-#         "Service": "lambda.amazonaws.com"
-#       },
-#       "Effect": "Allow"
-#     }
-#   ]
-# }
-# EOF
-# }
-
-# // Lambda permissions invoke function from S3
-# resource "aws_lambda_permission" "allow_bucket" {
-#   statement_id  = "AllowExecutionFromS3Bucket"
-#   action        = "lambda:InvokeFunction"
-#   function_name = aws_lambda_function.catalog-writer.arn
-#   principal     = "s3.amazonaws.com"
-#   source_arn    = "arn:aws:s3:::${var.bucket_name}"
-# }
+resource "aws_cloudwatch_log_group" "handler_lambda" {
+  name = "/aws/lambda/${aws_lambda_function.handler_api.function_name}-${var.env}"
+}
 
 # // tfstate output
-# output "lambda_arn" {
-#   value = aws_lambda_function.catalog-writer.arn
-# }
+output "lambda_arn" {
+  value = aws_lambda_function.handler_api.arn
+}

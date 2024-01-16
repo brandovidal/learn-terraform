11bf1ee77bde3acd054bcaa4a3a78f5c1fb54186
Brando Vidal <yisusdracon@gmail.com>
2024-01-16 07:40:59 -0500

    🔨  format variables.tf

--- projects/dev/services/backoffice/compute/lambda/variables.tf
+++ projects/dev/services/backoffice/compute/lambda/variables.tf
@@ -1,8 +1,8 @@
 variable "env" {}
 variable "env_name" {}
-variable "bucket_name"{}
-variable "backend_name"{}
-variable "lambda_role"{}
-variable "function_name"{}
-variable "apigateway_name"{}
-variable "handler_dir"{}
+variable "bucket_name" {}
+variable "backend_name" {}
+variable "lambda_role" {}
+variable "function_name" {}
+variable "apigateway_name" {}
+variable "handler_dir" {}
